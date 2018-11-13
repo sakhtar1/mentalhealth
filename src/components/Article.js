06ccd0892e2a9c8f5d0f446d505e172b95d3dedb
github username <github email address>
2018-11-13 11:03:29 -0500

    main files

---
+++ src/components/Article.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Article = () => {
+  return (
+    <div>
+      <h1> Article Page </h1>
+    </div>
+  );
+};
+
+export default Article;
---
+++ src/components/Health.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Health = () => {
+  return (
+    <div>
+      <h1> Health Page </h1>
+    </div>
+  );
+};
+
+export default Health;
