d2920c0781954c78d926bc08645108bb7167976d
github username <github email address>
2018-12-14 17:57:29 -0500

    mental health summary

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -8,11 +8,30 @@ const Homepage = () => {
 
 		  return (
 		    <div>
-		       <h1>Mental Health</h1>
+          <div>
+            <h1> Mental Health </h1>
+            </div>
+          <div>
+            <p> Mental disordered are believed by most people as rare and something that can nver happen to them
+            or their family members. However, mental disorders are very common with an estimated 54 million Americans 
+            suffering from a form of mental disorder in a given year. </p>
+            <p> It is hard on a most families to prepare and cope with this.
+            It can be physically and emotionally trying, and can make us feel vulnerable to the opinions and 
+            judgments of others.</p>
 
+            <p>If you think you or someone you know may have a mental or emotional problem, it is important to remember there is hope and help</p>
+            <p> To get more information about your or someone you love's mental health... </p>
 		       <p> Enter the zipcode for a mental health clinic near you: </p>
-				
-				<input id="zip-val" type="text" name="zip" maxlength="5" placeholder="00000" class="yellow" data-text="" />		       	
+          </div>
+          <div>
+    				<select id="placetype">
+              <option value="clinic">Mental Health Clinic</option>
+              <option value="hospital">Hospital</option>
+              <option value="urgentCare">Urgent Care</option>           
+            </select>
+      
+            <button>&nbsp;Search&nbsp;</button>
+          </div>
 		       <br/>
 		       <div>
 		       	<SimpleMap />
