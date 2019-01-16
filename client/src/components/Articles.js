23aec460932eb91075d5b4a93265f03de7c3a07a
github username <github email address>
2019-01-16 18:11:40 -0500

    adding rails api

---
+++ client/src/components/Articles.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+//import { Link } from 'react-router-dom';
+
+const Articles = () => {
+
+	// constructor(props) {
+ //    super(props);
+ //    this.state = {
+ //      value: 'Please write an essay about your favorite DOM element.'
+ //    };
+
+ //    this.handleChange = this.handleChange.bind(this);
+ //    this.handleSubmit = this.handleSubmit.bind(this);
+ //  }
+
+ //  handleChange(event) {
+ //    this.setState({value: event.target.value});
+ //  }
+
+ //  handleSubmit(event) {
+ //    alert('An essay was submitted: ' + this.state.value);
+ //    event.preventDefault();
+ //  }
+
+ //  return (
+ //    <div className="column">
+ //      <h1 className="title"> Article Page </h1> 
+ //       <form onSubmit={this.handleSubmit}>
+ //        <label>
+ //          Essay:
+ //          <textarea value={this.state.value} onChange={this.handleChange} />
+ //        </label>
+ //        <input type="submit" value="Submit" />
+ //      </form>  
+ //    </div>
+ //  );
+};
+
+export default Articles;
+
+
+
