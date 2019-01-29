dc3c5b018c6d69557e20aa4627c454bb3a78d073
github username <github email address>
2019-01-29 17:00:17 -0500

    client articles env

---
+++ client/build/precache-manifest.c57162580df0f1116f6bc6f9226e5cfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9ccc77714e47de51fd73",
+    "url": "/static/js/main.9ccc7771.chunk.js"
+  },
+  {
+    "revision": "3e0521ddbd225971d298",
+    "url": "/static/js/1.3e0521dd.chunk.js"
+  },
+  {
+    "revision": "9ccc77714e47de51fd73",
+    "url": "/static/css/main.14887c34.chunk.css"
+  },
+  {
+    "revision": "3e0521ddbd225971d298",
+    "url": "/static/css/1.415cafa6.chunk.css"
+  },
+  {
+    "revision": "12da7c6f5380f5330add75ef1abf0242",
+    "url": "/index.html"
+  }
+];
--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = process.env.APP_API_URL;
 
 
 export const login = user => {
