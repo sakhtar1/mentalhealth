01fe155ec9f47112db0131d1a0d1e70c1fb267fc
Sharmina Akhtar <sakht318@gmail.com>
2019-02-10 19:34:14 -0500

    MapstatetoProps change

--- client/src/reducers/articleLike.js
+++ client/src/reducers/articleLike.js
@@ -2,20 +2,15 @@ const articleLike = (state = [], action) => {
   switch (action.type){
     case 'LIKE_ARTICLE':
       let idx = state.findIndex(article => article.id === action.article.id)
-      // let newState = [...state.slice(0,idx), action.article, ...state.slice(idx+1,state.length+1)]
-
-      let articles = [ ...state ]
-      
+      let articles = [ ...state ]     
       if(idx !== -1) {
         articles[idx] = action.article
       } else {
         articles = [ action.article ]
       }
-   
-debugger
+      debugger
       return articles 
-
-  debugger
+      debugger
     default:
       return state
       debugger
