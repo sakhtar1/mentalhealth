442185a4b7cfcaeceb9b893adbe75d7f2399ee03
Sharmina Akhtar <sakht318@gmail.com>
2019-03-10 00:27:43 -0500

    Adding filter buttons

--- client/src/containers/Articles.js
+++ client/src/containers/Articles.js
@@ -9,22 +9,17 @@ import Article from '../components/Article'
 
 class Articles extends Component {
 
-	state = {
-		// when this component is mounted the intial state is an empty array
-		// then we fetch the articles from our backend and dispatch the articles
-		// to our reducer and update the reducer, bc we're returning this entire operation,
-		// after it is done, it'll allow us to hit this.setState then and by the time we hit this.setState
-		// then, the articles will be in the reduc store, and b/c we connected our component to the redux store
-		// we'll have access to those articles in the props
-		// and that's why we can say setState articles to this.props.articles
-		// because we're diplaying state by the time we hit renderarticles, the articles is the array from the store
-		// The array of articles in its default array. Then when we hit this.sortarticles it will sort
-		// this article from props, then we set this current state into the new state we want it to be
-		// we will sort the articles according to its created date, once its sorted we're going to
-		// tell the component I have a new value for your component it'll be the sorted array
-		
-		articles: []
-	}
+	constructor() {
+	    super();
+	    
+	    this.state = {
+	      articles: [],
+	      showMenu: false,
+	    }
+	    
+	    this.showMenu = this.showMenu.bind(this);
+	    this.closeMenu = this.closeMenu.bind(this);
+	  }
 
 	componentDidMount() {
 		this.props.fetchArticles().then(() => this.setState({articles: this.props.articles}))
@@ -49,11 +44,44 @@ class Articles extends Component {
 		let filterLike = this.props.articles.filter(article => article.likes > 0).sort(function(a, b) {
 			return a.likes - b.likes
 		})
-		//console.log(filterLike);
 		this.setState({articles: filterLike})
 		
 	}
-	
+
+	filterByLeastLikes = () => {
+		let filterLeast = this.props.articles.sort(function(a, b) {
+			return a.likes - b.likes
+		})
+		this.setState({articles: filterLeast})
+		
+	}
+
+	filterByMostLikes = () => {
+		let filterMost = this.props.articles.sort(function(a, b) {
+			return b.likes - a.likes
+		})
+		this.setState({articles: filterMost})
+		
+	}
+
+		showMenu = (event) => {
+		    event.preventDefault();
+		    
+		    this.setState({ showMenu: true }, () => {
+		      document.addEventListener('click', this.closeMenu);
+		    });
+		  }
+	  
+	  closeMenu = (event) => {
+	    
+	    if (!this.dropdownMenu.contains(event.target)) {
+	      
+	      this.setState({ showMenu: false }, () => {
+	        document.removeEventListener('click', this.closeMenu);
+	      });  
+	      
+	    }
+	  }
 
 	
 
@@ -64,7 +92,6 @@ class Articles extends Component {
 	}
 
 	render(){
-		//console.log(this.props.articles)
 		let renderArticles;
 		
 		if (this.state && this.state.articles.length !== 0){
@@ -87,7 +114,22 @@ class Articles extends Component {
 			<div>
 				<div>	
 					<button className='btn btn-secondary' onClick={() => this.clickToSort()}> Sort by Most Recent </button> 
-					<button id='filterlike' className='btn btn-secondary' onClick={() => this.filterByLikes()}> Filter by Likes </button>
+					  <br></br>
+					  <br></br>
+					  <div class="dropdown-trigger">
+				        <button class="button" aria-haspopup="true" aria-controls="dropdown-menu" onClick={this.showMenu}>
+				          Filter by:
+				        </button>      
+				        {this.state.showMenu ? (
+				              <div className="menu" ref={(element) => {this.dropdownMenu = element;}}>
+				                <button class="dropdown-item" onClick={() => this.clickToSort()}> Most Recent Article </button>
+				                <button class="dropdown-item" onClick={() => this.filterByLikes()}> Only Likes </button>
+				                <button class="dropdown-item" onClick={() => this.filterByMostLikes()}> Most Likes </button>
+				                <button class="dropdown-item" onClick={() => this.filterByLeastLikes()}> Least Likes </button>
+				              </div>
+				            ) : (null)}
+				      </div>
+			
 				</div>
 				{renderArticles}				
 			</div>
@@ -100,5 +142,6 @@ const mapStateToProps = state => {
 	  articles: state.allArticles.articles,
 	}
   }
+
 
 export default withRouter(connect(mapStateToProps, { fetchArticles, articleView })(Articles));
